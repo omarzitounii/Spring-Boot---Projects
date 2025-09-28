298f8e7886defd45f4124298f80e26caf95f4e5d
Omar Zitouni <zitouniomar2000@gmail.com>
2025-09-28 20:48:12 +0200

    Added first rest crud basics project

---
+++ basic_rest_crud/src/main/java/com/basics/springboot/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.basics.springboot.service;
+
+import com.basics.springboot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
